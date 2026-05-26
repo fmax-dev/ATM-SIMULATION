1570abe0866c0d3128fb640ffa686ca1651e83c0
fmax-dev <fmaxdev999@gmail.com>
2026-05-26 12:01:07 -0400

    (feat): Add interactive ATM menu with deposit, withdraw, balance check, and input validation.

--- atm.py
+++ atm.py
@@ -12,24 +12,61 @@ class ATM:
         """Allow users to deposit money to their account"""
         if amount > 0:
             self.balance += amount
-            print(f"\nSuccessfully deposited ${amount}.")
+            print(f"\nYou successfully deposited ${amount}.")
         elif amount == 0:
-            print(f"\nInvalid error: Amount must greater than 0")
+            print(f"\nInvalid error: Amount must greater than 0.")
         else:
-            print(f"\n${amount} must be positive")
+            print(f"\n${amount} must be positive.")
 
     def withdraw(self, amount):
         """Allow users to withdraw money from they account"""
         if amount > self.balance:
-            print("\nInsufficient amounts in your account")
+            print("\nYou have insufficient funds in your account.")
         elif amount <= 0:
-            print("\nWithdraw amount must positive or greater than 0")
+            print("\nWithdraw amount must positive or greater than 0.")
         else:
             self.balance -= amount
-            print(f"\n${amount} successfully withdrew")
+            print(f"\nYou successfully withdrew ${amount}.")
 
 def main():
-    pass
+    atm = ATM()
+
+    print("\n----- WELCOME TO THE ATM MACHINE -----")
+
+    # DISPLAYING MENU
+    while True:
+        print("\n--- ATM MENU ---")
+        print("    1. Check Balance")
+        print("    2. Deposit")
+        print("    3. Withdraw")
+        print("    4. Exit")
+
+        user_choice = input("\nPlease choose an option: ").strip()
+
+        # ROUTING LOGIC
+        if user_choice == "1":
+            atm.check_balance()
+        elif user_choice == "2":
+            while True:
+                try:
+                    deposit_amount = float(input("\nEnter amount to deposit: "))
+                    atm.deposit(deposit_amount)
+                    break
+                except ValueError:
+                    print("\nPlease enter a valid number")
+        elif user_choice == "3":
+            while True:
+                try:
+                    withdraw_amount = float(input("\nEnter amount to withdraw: "))
+                    atm.withdraw(withdraw_amount)
+                    break
+                except:
+                    print("\nPlease enter a valid number.")
+        elif user_choice == "4":
+            print("\nThank you for using the ATM!")
+            break
+        else:
+            print("\nInvalid choice! Please try again.")
 
 if __name__ == "__main__":
     main()
